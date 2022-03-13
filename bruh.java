369464c5188b4fc08fa3f2d2e1ecdc3fba5d04b4
Masih Ahmed <45457354+anon766@users.noreply.github.com>
2022-03-13 22:47:02 +0530

    Update bruh.java

--- bruh.java
+++ bruh.java
@@ -1,72 +1,22 @@
-Class KeyValue{
-	public String Key;
-	public String Value;
+
+
+if(check_node_availaibility(replication_Group_Id))
+{  
+    message_to_nodes=last_commited_Transaction_Id +" " +client_query;
+    System.out.println("\nTable "+table_name+" exists in database\nReplication Group "+replication_Group_Id+" available. Executing query");
+    response=send_transaction(message_to_nodes,replication_Group_Id);
+    if(!response.equals("error") && client_query.toLowerCase().contains("select")==false && check_node_availability(replication_Group_Id) != 3)
+        commit_transaction_to_log(client_query);
 }
 
 
-HashMap<String, String> KeyValue = new HashMap<>()
-
-
-class Btree {
-    public BTreeNode root;
-    public int t; 
- 
-    Btree(int t) {
-        this.root = null;
-        this.t = t;
-    }
- 
-    public void traverse() {
-        if (this.root != null)
-            this.root.traverse();
-        System.out.println();
-    }
- 
-    public BTreeNode search(int k) {
-        if (this.root == null)
-            return null;
-        else
-            return this.root.search(k);
-    }
-}
- 
-
-class BTreeNode {
-    int[] keys; 
-    int t; 
-    BTreeNode[] C;
-    int n;
-    boolean leaf;
- 
-    BTreeNode(int t, boolean leaf) {
-        this.t = t;
-        this.leaf = leaf;
-        this.keys = new int[2 * t - 1];
-        this.C = new BTreeNode[2 * t];
-        this.n = 0;
-    }
- 
-    public void traverse() {
- 		nt i = 0;
-        for (i = 0; i < this.n; i++) {
-            if (this.leaf == false) {
-                C[i].traverse();
-            }
-            System.out.print(keys[i] + " ");
-        }
- 		if (leaf == false)
-            C[i].traverse();
-    }
- 
-    BTreeNode search(int k) { 
-        int i = 0;
-        while (i < n && k > keys[i])
-            i++;
-        if (keys[i] == k)
-            return this;
-        if (leaf == true)
-            return null;
-        return C[i].search(k);
- 
-    }
-}
+int check_node_availaibility(int replication_Group_Id)
+{
+     int c=0;
+     for(int j=replication_Group_Id;j<9;j+=3)
+         if(CentralServer.available[j]==true)
+             c++;
+     if(c<2)
+         return 0;
+     return c;
+ }
